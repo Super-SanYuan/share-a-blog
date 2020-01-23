11e7f148c931d74d3a6c70f137d17c95744877a6
Ginger <346600397@qq.com>
2020-01-23 13:11:36 +0800

    add views

---
+++ src/views/BlogDetail/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    博客详情
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/EditBlog/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    编辑博客
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
