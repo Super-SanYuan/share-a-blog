fb00559104d0fc8ae183c6781a5f24c3c33724de
Ginger <346600397@qq.com>
2020-01-24 12:06:28 +0800

    add blog for api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,10 @@
 import request from '@/utils/request'
 
+/**
+ * 用户注册
+ * @param {*} username 用户名, 长度1到15个字符，只能是字母数字下划线中文
+ * @param {*} password 密码, 长度6到16个任意字符
+ */
 export const register = (username, password) => request({
   method: 'post',
   url: '/auth/register',
@@ -8,6 +13,11 @@ export const register = (username, password) => request({
   }
 })
 
+/**
+ * 用户登录
+ * @param {*} username 用户名, 长度1到15个字符，只能是字母数字下划线中文
+ * @param {*} password 密码, 长度6到16个任意字符
+ */
 export const login = (username, password) => request({
   method: 'post',
   url: '/auth/register',
@@ -16,11 +26,17 @@ export const login = (username, password) => request({
   }
 })
 
+/**
+ * 登出
+ */
 export const logout = () => request({
   method: 'get',
   url: '/auth/logout'
 })
 
+/**
+ * 判断用户是否登录
+ */
 export const getInfo = () => request({
   method: 'get',
   url: '/auth'
