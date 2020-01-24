cfe28f538cc203ee33eaeaf70eefc12784220ad7
sanyuan <sanyuan.jw@foxmail.com>
2020-01-24 16:44:36 +0800

    add Header&Footer for components

--- src/commons/components.js
+++ src/commons/components.js
@@ -2,8 +2,8 @@
  * 用于集中管理一些UI库的组件
  */
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Avatar } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
-
 Vue.use(Button)
+Vue.use(Avatar)
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    &copy; https://ginger-jiang.github.io 2020
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  padding: 4% 0;
+  background-color: #d1d1d1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+}
+</style>
