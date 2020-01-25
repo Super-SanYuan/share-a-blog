c6d6197ed66b6668bfafd59521eab05d8aff0abb
sanyuan <sanyuan.jw@foxmail.com>
2020-01-25 23:21:56 +0800

    add login&erg component

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,12 +1,100 @@
 <template>
-  <div>
-    登录
-  </div>
+  <section class="login-form">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="80px"
+    >
+      <el-form-item label="密码" prop="pass">
+        <el-input
+          type="password"
+          v-model="ruleForm.pass"
+          autocomplete="off"
+          placeholder="用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input
+          type="password"
+          v-model="ruleForm.checkPass"
+          autocomplete="off"
+          placeholder="密码"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >登录</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <p>没有账号？<router-link to="/reg">注册新用户</router-link></p>
+  </section>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
 </script>
-
-<style scoped>
+<style lang="less" scoped>
+.login-form {
+  width: 600px;
+  margin: 30px auto 0;
+}
+p {
+  margin: 50px auto 0;
+  text-align: center;
+  font-size: 12px;
+  a {
+    color: #092;
+    margin-left: 10px;
+  }
+}
 </style>
