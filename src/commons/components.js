95157a172a18ebffa29f892b939b769eaf2da264
Ginger <346600397@qq.com>
2020-01-25 23:21:56 +0800

    add login&erg component

--- src/commons/components.js
+++ src/commons/components.js
@@ -2,8 +2,11 @@
  * 用于集中管理一些UI库的组件
  */
 import Vue from 'vue'
-import { Button, Avatar } from 'element-ui'
+import { Button, Avatar, Form, FormItem, Input } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(Button)
 Vue.use(Avatar)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
--- src/views/Reg/index.vue
+++ src/views/Reg/index.vue
@@ -1,12 +1,98 @@
 <template>
-  <div>
-    注册
-  </div>
+  <el-form
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    ref="ruleForm"
+    label-width="80px"
+    class="reg-form"
+  >
+    <el-form-item label="密码" prop="pass">
+      <el-input
+        type="password"
+        v-model="ruleForm.pass"
+        autocomplete="off"
+        placeholder="用户名"
+      ></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+      <el-input
+        type="password"
+        v-model="ruleForm.checkPass"
+        autocomplete="off"
+        placeholder="密码"
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
+      <el-button @click="resetForm('ruleForm')">重置</el-button>
+    </el-form-item>
+
+    <p>已有账户?<router-link to="/login">立即登录</router-link></p>
+  </el-form>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
 </script>
-
-<style scoped>
+<style lang="less" scoped>
+.reg-form {
+  width: 600px;
+  margin: 30px auto 0;
+}
+p {
+  margin: 50px auto 0;
+  text-align: center;
+  font-size: 12px;
+  a {
+    color: #092;
+    margin-left: 10px;
+  }
+}
 </style>
