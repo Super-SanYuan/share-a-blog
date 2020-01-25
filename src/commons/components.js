9218ea825c0c86e13342ebeb06f01dee499413cd
sanyuan <sanyuan.jw@foxmail.com>
2020-01-25 23:46:36 +0800

    add home for pagination component

--- src/commons/components.js
+++ src/commons/components.js
@@ -2,7 +2,7 @@
  * 用于集中管理一些UI库的组件
  */
 import Vue from 'vue'
-import { Button, Avatar, Form, FormItem, Input } from 'element-ui'
+import { Button, Avatar, Form, FormItem, Input, Pagination } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(Button)
@@ -10,3 +10,4 @@ Vue.use(Avatar)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Pagination)
