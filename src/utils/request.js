254691e1d5dde0568268f14060047ad09089198f
Ginger <346600397@qq.com>
2020-01-27 15:53:27 +0800

    add qs format params

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import Qs from 'qs'
 import { Message } from 'element-ui'
+
 
 
 // 创建 axios 实例
@@ -7,7 +9,7 @@ const service = axios.create({
   baseURL: "http://blog-server.hunger-valley.com/", // 请求跟地址
   timeout: 1000,
   headers: { // 请求头
-    "content-type": "application/x-www-form-urlencoded"
+    "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
   },
   withCredentials: true // 异步请求也携带 token
 })
@@ -15,6 +17,7 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use(config => {
   // 需补充请求头的 token 携带
+  config.data = Qs.stringify(config.data)
   return config
 }, error => {
   return new Promise.reject(error)
@@ -23,9 +26,9 @@ service.interceptors.request.use(config => {
 // 响应拦截
 service.interceptors.response.use(response => {
   const httpStatus = Number(response.status) || 200 // http状态码
-  const resStatus = response.data.status // 后端定义的接口状态
+  // const resStatus = response.data.status // 后端定义的接口状态
   let resMessage // 后端定义的请求结果文字
-  if (httpStatus !== 200 || resStatus !== 'ok') {
+  if (httpStatus !== 200) {
     // http 状态码错误处理
     switch (httpStatus) {
       case 401:
@@ -50,6 +53,7 @@ service.interceptors.response.use(response => {
   }
   return response.data
 }, error => {
+  console.log(3);
   return new Promise.reject(new Error(error))
 })
 
