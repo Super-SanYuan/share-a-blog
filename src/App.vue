050ae8ad1e145929317fdac12c5913188f14fcb5
sanyuan <sanyuan.jw@foxmail.com>
2020-01-27 23:45:31 +0800

    updata app.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 <script>
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+// import { getInfo } from "@/api/auth";
 export default {
   name: "app",
   components: { Header, Footer }
