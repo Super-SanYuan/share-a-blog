b27b3db3f93e2d2c02dd55b79b00c880a9a376fd
Ginger <346600397@qq.com>
2020-01-27 23:29:35 +0800

    add public component

--- src/commons/components.js
+++ src/commons/components.js
@@ -2,8 +2,11 @@
  * 用于集中管理一些UI库的组件
  */
 import Vue from 'vue'
-import { Button, Avatar, Form, FormItem, Input, Pagination } from 'element-ui'
+import { Button, Avatar, Form, FormItem, Input, Pagination, Message, Dropdown, DropdownItem, DropdownMenu } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.prototype.$message = Message
 
 Vue.use(Button)
 Vue.use(Avatar)
@@ -11,3 +14,6 @@ Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Pagination)
+Vue.use(Dropdown)
+Vue.use(DropdownItem)
+Vue.use(DropdownMenu)
