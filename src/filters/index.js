a3353c1ca5d6be2f68456db17913562a55afa0e7
sanyuan <sanyuan.jw@foxmail.com>
2020-01-29 00:22:27 +0800

    add filters

---
+++ src/filters/index.js
@@ -0,0 +1,33 @@
+/**
+ * 获取指定时间距离此刻时间
+ * @param {*} date 时间
+ */
+const getDateDiff = (date) => {
+  // 判断是日期对象还是毫秒数
+  let newDate = typeof date === 'object' ? date : new Date(date)
+  let dateStr = newDate.getTime()
+  let now = Date.now() // 获取当前时间
+  let diff = now - dateStr
+
+  let res = ''
+
+  switch (true) {
+    case (diff < 6000):
+      res = '刚刚'
+      break
+    case (diff < 1000 * 3600):
+      res = Math.floor(diff / 60000) + '分钟前'
+      break
+    case (diff < 6000 * 3600 * 24):
+      res = Math.floor(diff / (1000 * 3600)) + '小时前'
+      break
+    default:
+      res = Math.floor(diff / (1000 * 3600 * 24)) + '天前'
+      break
+  }
+  return res;
+}
+
+export default {
+  getDateDiff
+}
