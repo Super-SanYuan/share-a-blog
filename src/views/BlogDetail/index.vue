778516a30acf21885665a526fe0421f11027a0a1
Ginger <346600397@qq.com>
2020-01-30 00:56:11 +0800

    complete blogDetail.vue

--- src/views/BlogDetail/index.vue
+++ src/views/BlogDetail/index.vue
@@ -1,12 +1,80 @@
 <template>
-  <div>
-    博客详情
+  <div class="blog-detail">
+    <section class="user">
+      <el-avatar
+        shape="square"
+        :size="60"
+        :src="userInfo.avatar || ''"
+      ></el-avatar>
+      <div>
+        <h2>{{ title }}</h2>
+        <span>
+          <router-link :to="`/user/${userInfo.id}`">{{
+            userInfo.username
+          }}</router-link>
+          发布于{{ createTime | getDateDiff }}</span
+        >
+      </div>
+    </section>
+    <section class="content" v-html="marked" />
   </div>
 </template>
 
 <script>
-export default {};
+import marked from "marked";
+import { getBlogDetail } from "@/api/blog";
+export default {
+  data() {
+    return {
+      title: "",
+      content: "",
+      blogId: this.$route.params.id,
+      createTime: undefined, // 发布时间
+      userInfo: {}
+    };
+  },
+  computed: {
+    marked() {
+      return marked(this.content);
+    }
+  },
+  created() {
+    this.getBlogInfo();
+  },
+  methods: {
+    async getBlogInfo() {
+      let blogInfo = await getBlogDetail(this.blogId);
+      this.userInfo = blogInfo.data.user;
+      this.title = blogInfo.data.title;
+      this.content = blogInfo.data.content;
+      this.createTime = blogInfo.data.createdAt;
+    }
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.blog-detail {
+  .user {
+    padding: 20px 0;
+    display: flex;
+    border-bottom: 1px solid #ccc;
+    > div {
+      margin-left: 20px;
+      display: flex;
+      flex-direction: column;
+      h2 {
+        margin: 0;
+      }
+      span {
+        margin: 4px 0;
+        color: #999;
+        font-size: 12px;
+        a {
+          color: #0094ff;
+        }
+      }
+    }
+  }
+}
 </style>
